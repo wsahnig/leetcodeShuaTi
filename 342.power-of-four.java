c80c2ab2b2eac8176ec1ba194aead5779c18b8e2
wsahnig <1215989376@qq.com>
2019-09-19 23:03:24 +0800

    git commit

--- 342.power-of-four.java
+++ 342.power-of-four.java
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-/*
- * @lc app=leetcode id=342 lang=java
- *
- * [342] Power of Four
- */
-class Solution {
-    public boolean isPowerOfFour(int num) {
-        
-    }
-}
-
-=======
 /*
  * @lc app=leetcode id=342 lang=java
  *
@@ -22,5 +9,3 @@ class Solution {
         && (num & 0x55555555) != 0;
     }
 }
-
->>>>>>> a47453625bbbdf482e37c42ed676631540a140d0
