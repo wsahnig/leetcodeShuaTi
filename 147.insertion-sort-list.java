6b5383e0b8e2aff96fa6a82bd4a083b011bf1443
wsahnig <1215989376@qq.com>
2019-10-04 22:11:40 +0800

    104

--- 147.insertion-sort-list.java
+++ 147.insertion-sort-list.java
@@ -12,7 +12,7 @@
  * }
  */
 class Solution {
-    public ListNode insertionSortList(ListNode head) {
+    public ListNode insertionSortList2(ListNode head) {
         if(head == null) return head;
         ListNode p = head.next,pre = head;
         while(p != null){
@@ -38,6 +38,30 @@ class Solution {
             p = next;
         }
         return head;
+    }
+    public ListNode insertionSortList(ListNode head) {
+        if(head == null) return head;
+        ListNode newHead = new ListNode(0);
+        newHead.next = head;
+        ListNode  p = head.next;
+        ListNode pre = head;
+        while(p != null){
+           
+           ListNode q = newHead;
+           while(q.next != null && q.next != p && q.next.val < p.val){
+                q = q.next;
+           }
+           //遍历结束后，q.next是不是就是第一个值大于p的节点或者就是p
+           if(q.next != p){
+               pre.next = p.next;
+               p.next = q.next;
+               q.next = p;
+           }else{
+               pre = p;
+           }
+           p = pre.next;
+        }
+        return newHead.next;
     }
 }
 
