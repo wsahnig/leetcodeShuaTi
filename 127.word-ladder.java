94c3a5dd57ebf6d9c0750cf42ab6ca917786e948
wsahnig <1215989376@qq.com>
2019-12-28 17:43:27 +0800

    1228

--- 127.word-ladder.java
+++ 127.word-ladder.java
@@ -4,8 +4,45 @@
  * [127] Word Ladder
  */
 class Solution {
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        
+    public int ladderLength(String s, String t, List<String> dic) {
+        HashMap<String,ArrayList<String>> dict = new HashMap();
+        for(String str : dic){
+            int len = str.length();
+            for(int i=0;i<len;i++){
+                String sub = str.substring(0,i)+"*"+str.substring(i+1);
+                ArrayList<String> value = dict.getOrDefault(sub,new ArrayList());    
+                value.add(str);
+                dict.put(sub,value);    
+            }
+        }
+        Queue<String> q = new LinkedList();
+        HashMap<String,Boolean> visited = new HashMap();
+        visited.put(s,true);
+        q.offer(s);
+        int level = 1;
+        while(!q.isEmpty()){
+            int size = q.size();
+            while(size-- > 0){
+                String cur = q.poll();
+                for(int i=0;i<cur.length();i++){
+                    String patt = cur.substring(0,i)+"*"+cur.substring(i+1);
+                    ArrayList<String> neighbors = dict.get(patt);
+                    if(neighbors != null){
+                        for(String neighbor : neighbors){
+                            if(!visited.containsKey(neighbor)){
+                                q.offer(neighbor);
+                                visited.put(neighbor,true);
+                                if(neighbor.equals(t)){
+                                    return level+1;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            level++;
+        }
+        return 0;
     }
 }
 
--- 78.subsets.java
+++ 78.subsets.java
@@ -4,19 +4,20 @@
  * [78] Subsets
  */
 class Solution {
-    List<List<Integer>> ans = new ArrayList<List<Integer>>();
-    private void backtrack(List<Integer> list,int[] nums,int start){
+    
+    private void backtrack(List<Integer> list,int[] nums,int start,List<List<Integer>> ans){
         ans.add(new ArrayList(list));
         for(int i=start;i<nums.length;i++){
           list.add(nums[i]);
-          backtrack(list,nums,i+1);
+          backtrack(list,nums,i+1,ans);
           list.remove(list.size()-1);
         }
     }
 
     public List<List<Integer>> subsets(int[] nums) {
         Arrays.sort(nums);
-        backtrack(new ArrayList(),nums,0);
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        backtrack(new ArrayList(),nums,0,ans);
         return ans;
     }
 }
