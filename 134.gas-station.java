a0a1a63d246db040ad3d2dcdfa74bf79454d192a
wsahnig <1215989376@qq.com>
2022-01-21 22:26:54 +0800

    改进134题的解法

--- 134.gas-station.java
+++ 134.gas-station.java
@@ -4,9 +4,28 @@
  * [134] Gas Station
  */
 class Solution {
-
-
     public int canCompleteCircuit(int[] gas, int[] cost) {
+        //逆向找 最大的sum和(为了走得尽可能远，以免车站左边无法抵达) 
+        //如果它都不能走完全程回到原点，那么这样的点就不存在。
+        int start = -1;
+        int sum = 0;
+        int max = -1;
+        for(int i = gas.length-1; i >= 0; i--)
+        {
+            int remain = (gas[i] - cost[i]);
+            sum += remain;
+            
+            if(sum > max)
+            {
+                start = i;
+                max = sum;
+            }
+            
+        }
+        return sum >= 0 ? start : -1;
+    }
+
+    public int canCompleteCircuit2(int[] gas, int[] cost) {
         int ans = -1;
         int i=0,j=0,k=0;
         int curr = 0;
