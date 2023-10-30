d51187be5dc0a3510976d69b8bd0e6abe0837418
wsahnig <1215989376@qq.com>
2023-10-30 21:06:04 +0800

    更新一个二分搜索解法

--- 275.h-index-ii.java
+++ 275.h-index-ii.java
@@ -5,21 +5,19 @@
  */
 class Solution {
     public int hIndex(int[] citations) {
-        int len = citations.length;
-        int cnt1 = 0,cnt2 = 0;
-        int tar = len;
-        int i = len-1;
-        while(i>=0){
-            while(i>=0 && citations[i] >= tar){
-                if(citations[i] > tar) cnt1++;
-                cnt2++;
-                i--;
-            }           
-            if(cnt1 <= tar && cnt2 >= tar) return tar;
-            tar--;
-            cnt1 = cnt2;
+        int left = 0, right = citations.length - 1;
+        //最小的索引left使得len - left >= citations[i]
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            int cnt = citations.length - mid;
+            if(cnt <= citations[mid]) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
         }
-        return 0;
+        return citations.length - left;
     }
 }
 
